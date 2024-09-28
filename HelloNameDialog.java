96167760ce997b032635d56a85a29a01297838ca
Trần Văn Đức Anh <khamvl08@gmail.com>
2024-09-28 11:18:16 +0700

    commit

---
+++ HelloNameDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class HelloNameDialog{
+	public static void main(String[] args){
+		String result;
+		result = JOptionPane.showInputDialog("Please enter your name:");
+		JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+		System.exit(0);
+	}
+}	
---
+++ Helloworld2.java
@@ -0,0 +1,6 @@
+public class Helloworld2{
+	public static void main(String args[]){
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	}
+}
