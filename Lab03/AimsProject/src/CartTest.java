c33672a15cf634a56f1a89409a5aa9759235d7b4
Duc Anh <khamvl08@gmail.com>
2024-11-23 11:14:11 +0700

    commit

---
+++ Lab03/AimsProject/src/CartTest.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class CartTest {
+    public static void main(String[] args) {
+        // Tạo một giỏ hàng mới
+        Cart cart = new Cart();
+        Scanner sc = new Scanner(System.in);
+
+        // Tạo các đối tượng DVD mới và thêm vào giỏ hàng
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                                                   "Animation",
+                                                   "Roger Allers",
+                                                   87,
+                                                   19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                                                   "Science Fiction",
+                                                   "George Lucas",
+                                                   87,
+                                                   24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin",
+                                                   "Animation",
+                                                   18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Kiểm tra phương thức in
+        cart.print();
+
+        // TODO: Kiểm tra các phương thức tìm kiếm ở đây
+        System.out.println("Enter the DVD you want to search (By title:)");
+        String searchedTitle = sc.nextLine();
+        cart.search(searchedTitle);
+
+        System.out.println("Enter the DVD you want to search (By id: )");
+        int searchedId = sc.nextInt();
+        cart.search(searchedId);
+        
+    }
+
+    
+}
---
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,85 @@
+public class DigitalVideoDisc {
+    private int id;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+    public String getDirector(){
+        return director;
+    }
+
+    public int getLength(){
+        return length;
+    }
+    public float getCost(){
+        return cost;
+    }
+    public DigitalVideoDisc(String title){
+        this.title = title;
+    }
+    
+    public DigitalVideoDisc(String title, String category, float cost ){
+        this.id = nbDigitalVideoDiscs++;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    
+    public DigitalVideoDisc(String title, String category,String director,int length, float cost){
+        this.id = nbDigitalVideoDiscs++;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+        this.length = length;
+    }   
+
+    public DigitalVideoDisc(String title, String category, String director, float cost){
+        this.id = nbDigitalVideoDiscs++;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+    }   
+
+    public boolean isMatch(String title){
+        return this.title.equals(title);
+    }
+}
