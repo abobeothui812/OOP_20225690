7801dca41190889e2457897726d12a5dde6043f9
Duc Anh <khamvl08@gmail.com>
2024-11-23 11:48:37 +0700

    passingparamter

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -32,6 +32,26 @@ public class DigitalVideoDisc {
         this.cost = cost;
     }
     
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
     public DigitalVideoDisc(String title, String category,String director,int length, float cost){
         this.title = title;
         this.category = category;
---
+++ Lab03/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,48 @@
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        // Tạo hai đối tượng DigitalVideoDisc
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        // Gọi hàm swap để đổi chỗ hai đối tượng (sẽ không có hiệu lực)
+        Rightswap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        // Gọi hàm changeTitle để thay đổi tiêu đề của jungleDVD
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    // Hàm swap: Thực hiện đổi chỗ hai đối tượng (nhưng không hiệu quả trong Java)
+    public static void Rightswap(DigitalVideoDisc Dvd1, DigitalVideoDisc Dvd2) {
+        DigitalVideoDisc tmp = new DigitalVideoDisc(Dvd1.getTitle(), Dvd1.getCategory(), Dvd1.getDirector(), Dvd1.getLength(), Dvd1.getCost());
+        
+        Dvd1.setTitle(Dvd2.getTitle());
+        Dvd1.setCategory(Dvd2.getCategory());
+        Dvd1.setDirector(Dvd2.getDirector());
+        Dvd1.setLength(Dvd2.getLength());
+        Dvd1.setCost(Dvd2.getCost());
+
+        Dvd2.setTitle(tmp.getTitle());
+        Dvd2.setCategory(tmp.getCategory());
+        Dvd2.setDirector(tmp.getDirector());
+        Dvd2.setLength(tmp.getLength());
+        Dvd2.setCost(tmp.getCost());
+        
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    // Hàm changeTitle: Thay đổi tiêu đề của một DigitalVideoDisc
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        // Tạo một đối tượng mới, nhưng không gán lại cho dvd
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
